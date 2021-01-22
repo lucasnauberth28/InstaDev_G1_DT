8811b04ae9e7605b985565870f1e6a25f859bb57
FIC <FIC@info.local>
2021-01-22 18:13:22 -0200

    First commit

---
+++ Edição de Perfil/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Projeto_Lucas_Nauberth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
