67a34069a3a3945ac58330e6cf66ae18998f4577
Regis Leandro da Silva Ribeiro Magalhães <engenheiro.regis.magalhaes@gmail.com>
2021-02-02 04:47:31 -0200

    Login criado

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,7 +1,21 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
 namespace InstaDev_G1_DT.Controllers
 {
-    public class LoginController
+    
+    [Route("Login")]
+    public class LoginController : Controller
     {
-        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Logar")]
+        public IActionResult Logar(IFormCollection form)
+        {
+            return LocalRedirect("~/");
+        }
     }
 }
