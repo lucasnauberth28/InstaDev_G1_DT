fe77b8cfcd39789f22ae13a5213397e29153809d
lucasnauberth28 <lucasnauberth@gmail.com>
2021-02-08 13:53:17 -0300

    Instadev

--- Controllers/EditProfileController.cs
+++ Controllers/EditProfileController.cs
@@ -2,6 +2,8 @@ using System;
 using InstaDev_G1_DT.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Collections.Generic;
 
 namespace InstaDev_G1_DT.Controllers
 {
@@ -30,6 +32,64 @@ namespace InstaDev_G1_DT.Controllers
             // localhost:5001/User/EditProfile
             return LocalRedirect("~/EditProfile");
 
+        }
+
+        
+        [Route("EditarPerfil-Usuario")]
+        public User MostrarUsuario()
+        {
+            var userid = HttpContext.Session.GetString("_UserId");
+            User userLogado = userModels.BuscarUsuarioPorId(int.Parse(userid));
+
+            return userLogado;
+        }
+        [Route("EditarPerfil/SwitchData")]
+        public IActionResult SwitchData(IFormCollection form)
+        {
+            User newUser = MostrarUsuario();
+            newUser.CompleteName = form["Name"];
+            newUser.Photo = form["Photo"];
+            
+            if(form.Files.Count > 0){
+
+                var file = form.Files[0];
+                var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/perfil");
+
+                if(!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/", folder, file.FileName);
+
+                using(var stream = new FileStream(path, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+                
+                newUser.Photo = file.FileName;           
+            }
+
+            newUser.Email = form["Email"];
+            newUser.UserName = form["Username"];
+            
+            userModels.Update(newUser);
+
+            ViewBag.UsuarioAtualizado = newUser;
+
+            return LocalRedirect("~/EditarPerfil");
+        }
+
+        [Route("ExcluirPerfil")]
+        public IActionResult Excluir(int id)
+        {
+            var userId = HttpContext.Session.GetString("_UserId");
+            if (userId == id.ToString())
+            {
+                userModels.Delete(id);
+
+            }
+            return LocalRedirect("~/Login");
         }
 
     }
